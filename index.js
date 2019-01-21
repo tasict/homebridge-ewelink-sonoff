229d913fffeba861a29a30357d6fae190408c30b
howang <howanghk@gmail.com>
2019-01-21 20:31:22 +0800

    resolve device channel count from uiid

--- index.js
+++ index.js
@@ -152,24 +152,12 @@ function eWeLink(log, config, api) {
 
                             let accessory = platform.accessories.get(deviceId);
                             let deviceInformationFromWebApi = platform.devicesFromApi.get(deviceId);
-                            let switchesAmount = 0;
+                            let switchesAmount = platform.getDeviceChannelCount(deviceInformationFromWebApi);
 
                             accessory.getService(Service.AccessoryInformation).setCharacteristic(Characteristic.SerialNumber, deviceInformationFromWebApi.extra.extra.mac);
                             accessory.getService(Service.AccessoryInformation).setCharacteristic(Characteristic.Manufacturer, deviceInformationFromWebApi.productModel);
-                            accessory.getService(Service.AccessoryInformation).setCharacteristic(Characteristic.Model, deviceInformationFromWebApi.extra.extra.model);
+                            accessory.getService(Service.AccessoryInformation).setCharacteristic(Characteristic.Model, deviceInformationFromWebApi.extra.extra.model + ' (' + deviceInformationFromWebApi.uiid + ')');
                             accessory.getService(Service.AccessoryInformation).setCharacteristic(Characteristic.FirmwareRevision, deviceInformationFromWebApi.params.fwVersion);
-
-                            switch(deviceInformationFromWebApi.extra.extra.model) {
-                                case 'PSF-B04-GL' :
-                                    switchesAmount = 3;
-                                    break;
-                                case 'PSB-B04-GL' :
-                                    switchesAmount = 2;
-                                    break;
-                                case 'PSF-A04-GL' :
-                                    switchesAmount = 4;
-                                    break;
-                            }
 
                             if(switchesAmount > 1) {
                                 platform.log(switchesAmount + " channels device has been set: " + deviceInformationFromWebApi.extra.extra.model);
@@ -185,19 +173,7 @@ function eWeLink(log, config, api) {
 
                         } else {
                             let deviceToAdd = platform.devicesFromApi.get(deviceId);
-                            let switchesAmount = 1;
-
-                            switch(deviceToAdd.extra.extra.model) {
-                                case 'PSF-B04-GL' :
-                                    switchesAmount = 3;
-                                    break;
-                                case 'PSB-B04-GL' :
-                                    switchesAmount = 2;
-                                    break;
-                                case 'PSF-A04-GL' :
-                                    switchesAmount = 4;
-                                    break;
-                            }
+                            let switchesAmount = platform.getDeviceChannelCount(deviceToAdd);
 
                             if(switchesAmount > 1) {
                                 for(let i=0; i!==switchesAmount; i++) {
@@ -405,16 +381,9 @@ eWeLink.prototype.addAccessory = function(device, deviceId = null) {
     accessory.getService(Service.AccessoryInformation).setCharacteristic(Characteristic.Identify, false);
     accessory.getService(Service.AccessoryInformation).setCharacteristic(Characteristic.FirmwareRevision, device.params.fwVersion);
 
-    switch(device.extra.extra.model) {
-        case 'PSF-B04-GL' :
-             accessory.context.switches = 3;
-            break;
-        case 'PSB-B04-GL' :
-            accessory.context.switches = 2;
-            break;
-        case 'PSF-A04-GL' :
-            accessory.context.switches = 4;
-            break;
+    let switchesAmount = platform.getDeviceChannelCount(device);
+    if (switchesAmount > 1) {
+        accessory.context.switches = switchesAmount;
     }
 
     this.accessories.set(device.deviceid, accessory);
@@ -760,6 +729,96 @@ eWeLink.prototype.relogin = function (callback) {
         }
         callback && callback();
     });
+};
+
+eWeLink.prototype.getDeviceTypeByUiid = function (uiid) {
+    const MAPPING = {
+        1: "SOCKET",
+        2: "SOCKET_2",
+        3: "SOCKET_3",
+        4: "SOCKET_4",
+        5: "SOCKET_POWER",
+        6: "SWITCH",
+        7: "SWITCH_2",
+        8: "SWITCH_3",
+        9: "SWITCH_4",
+        10: "OSPF",
+        11: "CURTAIN",
+        12: "EW-RE",
+        13: "FIREPLACE",
+        14: "SWITCH_CHANGE",
+        15: "THERMOSTAT",
+        16: "COLD_WARM_LED",
+        17: "THREE_GEAR_FAN",
+        18: "SENSORS_CENTER",
+        19: "HUMIDIFIER",
+        22: "RGB_BALL_LIGHT",
+        23: "NEST_THERMOSTAT",
+        24: "GSM_SOCKET",
+        25: "AROMATHERAPY",
+        26: "BJ_THERMOSTAT",
+        27: "GSM_UNLIMIT_SOCKET",
+        28: "RF_BRIDGE",
+        29: "GSM_SOCKET_2",
+        30: "GSM_SOCKET_3",
+        31: "GSM_SOCKET_4",
+        32: "POWER_DETECTION_SOCKET",
+        33: "LIGHT_BELT",
+        34: "FAN_LIGHT",
+        35: "EZVIZ_CAMERA",
+        36: "SINGLE_CHANNEL_DIMMER_SWITCH",
+        38: "HOME_KIT_BRIDGE",
+        40: "FUJIN_OPS",
+        41: "CUN_YOU_DOOR",
+        42: "SMART_BEDSIDE_AND_NEW_RGB_BALL_LIGHT",
+        43: "",
+        44: "",
+        45: "DOWN_CEILING_LIGHT",
+        46: "AIR_CLEANER",
+        49: "MACHINE_BED",
+        51: "COLD_WARM_DESK_LIGHT",
+        52: "DOUBLE_COLOR_DEMO_LIGHT",
+        53: "ELECTRIC_FAN_WITH_LAMP",
+        55: "SWEEPING_ROBOT",
+        56: "RGB_BALL_LIGHT_4",
+        57: "MONOCHROMATIC_BALL_LIGHT",
+        59: "MEARICAMERA",
+        1001: "BLADELESS_FAN",
+        1002: "NEW_HUMIDIFIER",
+        1003: "WARM_AIR_BLOWER"
+    };
+    return MAPPING[uiid] || "";
+};
+
+eWeLink.prototype.getDeviceChannelCountByType = function (deviceType) {
+    const DEVICE_CHANNEL_LENGTH = {
+        SOCKET: 1,
+        SWITCH_CHANGE: 1,
+        GSM_UNLIMIT_SOCKET: 1,
+        SWITCH: 1,
+        THERMOSTAT: 1,
+        SOCKET_POWER: 1,
+        GSM_SOCKET: 1,
+        POWER_DETECTION_SOCKET: 1,
+        SOCKET_2: 2,
+        GSM_SOCKET_2: 2,
+        SWITCH_2: 2,
+        SOCKET_3: 3,
+        GSM_SOCKET_3: 3,
+        SWITCH_3: 3,
+        SOCKET_4: 4,
+        GSM_SOCKET_4: 4,
+        SWITCH_4: 4,
+        CUN_YOU_DOOR: 4
+    };
+    return DEVICE_CHANNEL_LENGTH[deviceType] || 0;
+};
+
+eWeLink.prototype.getDeviceChannelCount = function (device) {
+    let deviceType = this.getDeviceTypeByUiid(device.uiid);
+    //this.log('Device type for %s is %s', device.uiid, deviceType);
+    let channels = this.getDeviceChannelCountByType(deviceType);
+    return channels;
 };
 
 /* WEB SOCKET STUFF */
