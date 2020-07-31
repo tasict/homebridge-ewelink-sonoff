324897db0ff0db1745687b8cf2aeeba59a03d0ad
bwp91 <43026681+bwp91@users.noreply.github.com>
2020-07-31 10:16:15 +0100

    package name to constant

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /* jshint esversion: 9, -W030, node: true */
 "use strict";
+const constants = require("./lib/constants");
 module.exports = function (homebridge) {
    let eWeLink = require("./lib/eWeLink.js")(homebridge);
-   homebridge.registerPlatform("homebridge-ewelink-sonoff", "eWeLink", eWeLink, true);
+   homebridge.registerPlatform(constants.packageName, "eWeLink", eWeLink, true);
 };
